8e9147a5cef81febae565ddca504cc87cfaf4b69
weihongwei <hwwei@thoughtworks.com>
2016-11-17 10:54:32 +0800

    使用GrizzlyHttpServer启动jersey

--- src/main/java/com/tw/study/jersey/App.java
+++ src/main/java/com/tw/study/jersey/App.java
@@ -1,13 +1,41 @@
 package com.tw.study.jersey;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static final URI BASE_URI = URI.create("http://localhost:8080/base/");
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("\"Hello World\" Jersey Example App");
+
+            final ResourceConfig resourceConfig = new ResourceConfig();
+            resourceConfig.packages("com.tw.study.jersey");
+            final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, resourceConfig, false);
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    server.shutdownNow();
+                }
+            }));
+            server.start();
+
+            System.out.println(String.format("Application started.\nTry out %s\nStop the application using CTRL+C",
+                    BASE_URI));
+            Thread.currentThread().join();
+        } catch (IOException | InterruptedException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 }
